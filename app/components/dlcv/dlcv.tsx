08dbb5a610c048a8eabc2668b25367b5a1ebe3a2
Antoine Mistral <antoinemistral.dev@gmail.com>
2025-03-11 17:42:38 +0100

    Correction 2

--- app/components/dlcv/dlcv.tsx
+++ app/components/dlcv/dlcv.tsx
@@ -3,7 +3,7 @@ import "./dlcv.sass"
 const DownloadCV = () => {
   return (
     <div className="cv-container">
-      <a href="/cv.pdf" download="Mistral.AntoineCV.pdf" className="cv-button">
+      <a href="/cv.pdf" download="Mistral.Antoine.CV.pdf" className="cv-button">
         Télécharger mon CV
       </a>
     </div>
--- app/components/projectcard/projectcard.tsx
+++ app/components/projectcard/projectcard.tsx
@@ -13,6 +13,7 @@ const Projects = () => {
             <img src={project.image} alt={project.title} className="project-image" />
             <h3>{project.title}</h3>
             <p>{project.description}</p>
+            <p>{project.langage}</p>
             <div className="project-links">
               <a href={project.liveUrl} target="_blank" rel="noopener noreferrer">
                 Voir le projet
