4e18a317eaca762c508330120b9d16145a4dcb7f
Antoine Mistral <antoinemistral.dev@gmail.com>
2025-03-15 05:44:19 +0100

    Changement CV

--- app/components/dlcv/dlcv.tsx
+++ app/components/dlcv/dlcv.tsx
@@ -3,7 +3,7 @@ import "./dlcv.sass"
 const DownloadCV = () => {
   return (
     <div className="cv-container">
-      <a href="/cv.pdf" download="Mistral.Antoine.CV.pdf" className="cv-button">
+      <a href="/Mistral.Antoine.CV.pdf" download="Mistral.Antoine.CV.pdf" className="cv-button">
         Télécharger mon CV
       </a>
     </div>
